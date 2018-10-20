6b20a37b00e35a9416fcda3f4d7a5e490bbaf99c
Priya Mangal <mangalpriya67@gmail.com>
2018-10-20 15:32:33 -0700

    Initial React app setup

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Login from './login';
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(<Login />, document.getElementById('main'));
+});
